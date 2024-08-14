5516b438a136168d1ba962ee8312977e0b9a98eb
msopiann <muhammadsopian452@gmail.com>
2024-08-14 20:55:48 +0700

    feat(monitoring): change to hardcode markdown for tabel

--- components/TabelDokumentasi.tsx
+++ components/TabelDokumentasi.tsx
@@ -1,51 +1,73 @@
-// TabelDokumentasi.tsx
 import React from "react";
 import { data } from "../lib/data";
 
 const TabelDokumentasi: React.FC = () => {
   return (
-    <table className="min-w-full divide-y divide-gray-200">
-      <thead className="bg-gray-50">
-        <tr>
-          <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border border-gray-300">
-            Nama
-          </th>
-          <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border border-gray-300">
-            Jalur
-          </th>
-          <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border border-gray-300">
-            Judul
-          </th>
-          <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border border-gray-300">
-            Proposal
-          </th>
-          <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border border-gray-300">
-            Sidang
-          </th>
-        </tr>
-      </thead>
-      <tbody className="bg-white divide-y divide-gray-200">
-        {data.map((item, index) => (
-          <tr key={index}>
-            <td className="px-6 py-4 whitespace-nowrap border border-gray-300">
-              {item.nama}
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap border border-gray-300">
-              {item.jalur}
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap border border-gray-300">
-              {item.judul}
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap border border-gray-300">
-              {item.proposal}
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap border border-gray-300">
-              {item.sidang}
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <div className="flex flex-col">
+      <div className="-m-1.5 overflow-x-auto">
+        <div className="p-1.5 min-w-full inline-block align-middle">
+          <div className="overflow-hidden">
+            <table className="min-w-full divide-y divide-gray-200">
+              <thead>
+                <tr>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase"
+                  >
+                    Nama
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase"
+                  >
+                    Jalur
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase"
+                  >
+                    Judul
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-end text-xs font-medium text-gray-500 uppercase"
+                  >
+                    Sempro
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-end text-xs font-medium text-gray-500 uppercase"
+                  >
+                    Akhir
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="divide-y divide-gray-200">
+                {data.map((item, index) => (
+                  <tr key={index}>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800">
+                      {item.nama}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800">
+                      {item.jalur}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800">
+                      {item.judul}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-end text-sm font-medium">
+                      {item.proposal}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-end text-sm font-medium">
+                      {item.sidang}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
